f4ed9905e713ffc4fe7f089ae03bac5986c5625e
Salma Assem <salmaassem95@gmail.com>
2026-06-04 01:20:45 +0200

    add dashboard for doctor and nurse and it's context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./app/context/AuthContext";
 import SignIn from "./app/pages/auth/SignIn";
 import SignUp from "./app/pages/auth/SignUp";
 import ForgotPassword from "./app/pages/auth/ForgotPassword";
 import ResetPassword from "./app/pages/auth/ResetPassword";
+import { Dashboard } from "./app/pages/Doctor side/Dashboard";
+
 
 function App() {
   return (
+     <AuthProvider>
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
@@ -17,7 +21,14 @@ function App() {
         path="/reset-password"
         element={<ResetPassword />}
       />
+      <Route
+        path="/dashboard"
+        element={<Dashboard />}
+      />
+      
+
     </Routes>
+    </AuthProvider>
   );
 }
 
