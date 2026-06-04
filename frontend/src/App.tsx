5e124775e7cb5d0eab5e0b9589ceaf8288d00b60
unknown <you@example.com>
2026-06-04 18:51:26 +0300

    feat: integrate navbar, footer, and added mainlayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,22 @@ import SignUp from "./app/pages/auth/SignUp";
 import ForgotPassword from "./app/pages/auth/ForgotPassword";
 import ResetPassword from "./app/pages/auth/ResetPassword";
 import { Dashboard } from "./app/pages/Doctor side/Dashboard";
-
+import MainLayout from "./app/Layouts/MainLayout";
 
 function App() {
   return (
-     <AuthProvider>
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
-      <Route
-        path="/reset-password"
-        element={<ResetPassword />}
-      />
-      <Route
-        path="/dashboard"
-        element={<Dashboard />}
-      />
-      
-
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route element={<MainLayout />}>
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </AuthProvider>
   );
 }
