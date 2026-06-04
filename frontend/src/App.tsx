a6d9f828c91887dfa0d6ea514335381cfbe9877f
walaa.khafagy <walaa.a.khafagy@gmail.com>
2026-06-04 19:44:31 +0300

    feat: create patient profile page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,20 @@ import SignUp from "./app/pages/auth/SignUp";
 import ForgotPassword from "./app/pages/auth/ForgotPassword";
 import ResetPassword from "./app/pages/auth/ResetPassword";
 import { Dashboard } from "./app/pages/Doctor side/Dashboard";
-
+import { PatientProfile } from "./app/pages/patient/PatientProfile";
 
 function App() {
   return (
-     <AuthProvider>
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
-      <Route
-        path="/reset-password"
-        element={<ResetPassword />}
-      />
-      <Route
-        path="/dashboard"
-        element={<Dashboard />}
-      />
-      
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />}/>
+        <Route path="/reset-password" element={<ResetPassword />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/patient-profile" element={<PatientProfile />} />
 
-    </Routes>
+      </Routes>
     </AuthProvider>
   );
 }
