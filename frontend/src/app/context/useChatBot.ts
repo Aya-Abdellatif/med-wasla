fe40021859706f881849d3c0363f7c234d12e09e
HanaF02 <hanaahmedsamir@gmail.com>
2026-06-10 21:30:27 +0300

    fix import

--- frontend/src/app/context/useChatBot.ts
+++ frontend/src/app/context/useChatBot.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ChatBotContext } from "./ChatBotContextProvider";
+import { ChatBotContext } from "./ChatBotContextProvider.ts";
 
 export function useChatBot() {
   const context = useContext(ChatBotContext);
