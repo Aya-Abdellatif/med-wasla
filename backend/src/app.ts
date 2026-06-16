0b72f61c91360a10866784f06e1555dd2a556042
mayar2001731 <mayaroraby77@gmail.com>
2026-06-16 20:26:34 +0300

    create queue.test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import errorHandler from "./middleware/errorHandler.middleware.js";
 import authRouter from "./features/auth/auth.route.js";
 import specialistsRouter from "./features/medicalSpecialist/Doctors/specialists.routes.js";
 import reviewsRouter from "./features/reviews/reviews.routes.js";
+import queueRouter from "./features/queue/queue.routes.js";
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/patient", patientRouter);
 app.use("/api/specialists", specialistsRouter);
 // app.use("/api/appointments", appointmentsRouter);
-// app.use("/api/queue", queueRouter);
+ app.use("/api/queue", queueRouter);
 app.use("/api/reviews", reviewsRouter);
 // app.use("/api/admin", adminRouter);
 // app.use("/api/ai", aiRouter);
